eb9827844ff4d8af7113d2e4f923b387542f9a1e
izu457 <praecil@gmail.com>
2024-07-17 12:09:50 +0200

    cleaned file and outsourced functions to helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -72,8 +72,8 @@ def get_majorities(df, threshold):
     Takes a DataFrame and a threshold. The threshold defines which percentage of votes
     of biggest left- and biggest right-leaning party group is needed to be considered a 
     left-leaning or right-leaning majority.
-    Returns a DataFrame with columns for each majority type (Consensus, RM, LM) and a
-    binary value for each vote indicating if it is part of the majority.
+    Returns a DataFrame with columns for each majority type (General, LM, RM) 
+    and a binary value for each vote indicating if it is part of the majority.
     """
     # Create new DataFrame columns as calculations (not modifying the original df)
     majority = (df['ECR%'] >= threshold) & (df['S&D%'] >= threshold)
@@ -218,59 +218,6 @@ def extract_summary_links(urls):
     # Print final count on a new line once all URLs are processed
     print(f"\nTotal links successfully extracted {len(summary_links) - summary_links.count("NA")} out of {len(summary_links)}")
     return summary_links
-
-# def fetch_summary_link(url, session):
-#     """
-#     Fetches HTML content for a given URL, parses it, and extracts the summary link.
-#     """
-#     try:
-#         # Get HTML from the URL using a session object for better performance
-#         response = session.get(url)
-#         response.raise_for_status()  # Raises an HTTPError for bad responses
-#         # Parse HTML
-#         soup = BeautifulSoup(response.text, 'html.parser')
-#         # Find the "button" element with id "summary"
-#         button = soup.find("button", {"id": "summary"})
-#         # print number of elements in button
-#         print(f"Number of elements in button: {len(button)}")
-#         if button and 'onclick' in button.attrs:
-#             # Extract the link directly from the onclick attribute
-#             onclick_content = button['onclick'].split("'")[1]
-#             summary_link = "https://oeil.secure.europarl.europa.eu" + onclick_content
-#             #print(f"Extracted summary link: {summary_link}")
-#             return summary_link
-#         return None
-#     except requests.RequestException as e:
-#         print(f"Failed to retrieve or parse {url}: {e}")
-#         return "NA"
-
-# def extract_summary_links(urls):
-#     """
-#     Processes a list of URLs in parallel to extract summary links.
-#     Source: https://docs.python.org/3/library/concurrent.futures.html, Example
-#     """
-#     summary_links = []
-#     # Use requests.Session for connection pooling
-#     with requests.Session() as session:
-#         # Use ThreadPoolExecutor to execute requests in parallel
-#         with ThreadPoolExecutor(max_workers=10) as executor:
-#             # Map fetch_summary_link function over all URLs with the session passed
-#             future_to_url = {executor.submit(fetch_summary_link, url, session): url for url in urls}
-#             for future in concurrent.futures.as_completed(future_to_url):
-#                 url = future_to_url[future]
-#                 try:
-#                     result = future.result()
-#                     if result:
-#                         summary_links.append(result)
-#                         print(f"Links extracted: {len(summary_links)}", end='\r')
-#                     else:
-#                         summary_links.append("NA")
-#                 except Exception as exc:
-#                     print(f'{url} generated an exception: {exc}')
-#                     summary_links.append("NA")
-#     # Print final count on a new line once all URLs are processed
-#     print("\nTotal links extracted:", len(summary_links))
-#     return summary_links
     
 def fetch_summary_texts(url):
     """
@@ -318,46 +265,95 @@ def extract_summary_texts(urls):
     print(f"\nTotal summaries successfully extracted {len(summaries) - summaries.count("NA")} out of {len(summaries)}")
     return summaries
 
-# def fetch_summary_texts(url, session):
-#     """
-#     Fetch the content of a URL and process the HTML to extract cleaned summary text.
-#     """
-#     try:
-#         response = session.get(url)
-#         soup = BeautifulSoup(response.text, 'html.parser')
-#     # Find the element using a regex to flexibly match the string
-#         summary_elements = soup.find_all('span', lang="EN-GB")
-#         if summary_elements:
-#             summary_text = " ".join(re.sub(r'\s+', ' ', item.text.strip()) for item in summary_elements)
-#             return summary_text
-#         return None
-#     except requests.RequestException as e:
-#         print(f"Failed to retrieve or parse {url}: {e}")
-#         return "NA"
+def get_random_guess(data):
+    """
+    Creates a random guess for the group percentages and majorities for each vote.
+    Returns dataframe with random guesses.
+    """
+    # create new dataframe with same index/ Vote IDs
+    data = pd.DataFrame(index=data.index)
+    # add random guesses between 0 and 1 rounded to two decimals for each group
+    data['ECR%'] = [round(random.random(), 2) for _ in range(len(data))]
+    data['EPP%'] = [round(random.random(), 2) for _ in range(len(data))]
+    data['Greens/EFA%'] = [round(random.random(), 2) for _ in range(len(data))]
+    data['EFD/IDG%'] = [round(random.random(), 2) for _ in range(len(data))]
+    data['NI%'] = [round(random.random(), 2) for _ in range(len(data))]
+    data['REG%'] = [round(random.random(), 2) for _ in range(len(data))]
+    data['S&D%'] = [round(random.random(), 2) for _ in range(len(data))]
+    data['The Left%'] = [round(random.random(), 2) for _ in range(len(data))]
+    # guess 0 or 1 for majorities
+    data['General Majority'] = [random.choice([0, 1]) for _ in range(len(data))]
+    data['Right Majority'] = [random.choice([0, 1]) for _ in range(len(data))]
+    data['Left Majority'] = [random.choice([0, 1]) for _ in range(len(data))]
+    data['Consensus'] = [random.choice([0, 1]) for _ in range(len(data))]
+    return data
 
-# def extract_summary_texts(urls):
-#     """
-#     Takes a list of urls and extracts the summary text from the html of the page
-#     by combining all the paragraphs of the summary into one string.
-#     Returns a list of summary texts.
-#     """
-#     summaries = []
-#     # Create a session object
-#     with requests.Session() as session:
-#         # Use ThreadPoolExecutor to execute requests in parallel
-#         with ThreadPoolExecutor(max_workers=10) as executor:
-#             # executor class should only be used via subclass such as map
-#             # Map lets fetch_and_process function be executed asynchronously with several calls in parallel
-#             future_to_url = {executor.submit(fetch_summary_texts, url, session): url for url in urls}
-#             for future in concurrent.futures.as_completed(future_to_url):
-#                 url = future_to_url[future]
-#                 try:
-#                     results = future.result()
-#                     if results:
-#                         summaries.append(results)
-#                         print(f"Summaries extracted: {len(summaries)}", end='\r')
-#                     else:
-#                         summaries.append("NA")
-#                 except Exception as exc:
-#                         print(f'{url} generated an exception: {exc}')
-#     return summaries
+def get_accuracy_df (original_df, test_df, tolerance_interval):
+    """"
+    Function to calculate the accuracy of a test dataframe compared to an original dataframe.
+    Takes as arguments the original dataframe and the test dataframe and a tolerance interval.
+    For each index in the original data frame, it finds the corresponding index in the test data frame
+    and checks if the values in the test data frame are within the tolerance interval of the original 
+    data frame.
+    Calculates accuracy as the number of correct guesses divided by the number of guesses.
+    Returns the accuracy.
+    """
+    # try to set index of test dataframe to Vote ID if not already done
+    try:
+        test_df.set_index('Vote ID', inplace=True)
+    except:
+        pass
+    # create a new dataframe with the same index as the original dataframe
+    accuracy_df = pd.DataFrame(index=test_df.index)
+    # add a column for each column in the original dataframe
+    for column in test_df.columns:
+        accuracy_df[column] = 0
+    # loop through the rows of the test dataframe
+    for index in test_df.index:
+        # loop through the columns of the test dataframe
+        for column in test_df.columns:
+            # Debugging: Print the types and values
+            #print(f"Index: {index}, Column: {column}")
+            #print(f"Original DF Value: {original_df.loc[index, column]}, Type: {type(original_df.loc[index, column])}")
+            #print(f"Test DF Value: {test_df.loc[index, column]}, Type: {type(test_df.loc[index, column])}")
+            
+            # check if the value in the test dataframe is within the tolerance interval of the original dataframe
+            if ((original_df.loc[index, column] - tolerance_interval) <= test_df.loc[index, column]) & (test_df.loc[index, column] <= (original_df.loc[index, column] + tolerance_interval)):                # if it is, add 1 to the corresponding cell in the accuracy dataframe
+                accuracy_df.loc[index, column] = 1
+    # calculate the accuracy as the sum of the accuracy dataframe divided by the number of cells
+    accuracy = accuracy_df.sum().sum() / (accuracy_df.shape[0] * accuracy_df.shape[1])
+    return round(accuracy, 4), accuracy_df
+
+def process_session(voted_docs, party_names, majority_names):
+    """
+    Combines the functions get_random_guess and get_accuracy_df to calculate the accuracy 
+    of a random guess for party and majority votes individually.
+    Takes as arguents a dataframe of voted documents and the names of the columns for the 
+    party and majority votes.
+    Returns the accuracy for party and majority votes.
+    """
+    random_guess = get_random_guess(voted_docs)
+    random_party = get_accuracy_df(voted_docs, random_guess[party_names], 0.05)
+    random_majority = get_accuracy_df(voted_docs, random_guess[majority_names], 0.05)
+    return random_party[0], random_majority[0]
+
+def split_select_data(data, training_fraction):
+    """"
+    Function to split the data into training and testing and to drop unnecessary columns.
+    Takes as arguments the data and the fraction of the data to be used for training.
+    Returns the training and testing data.
+    """
+    # create a copy of the data
+    data = data.copy()
+    # drop unnecessary columns
+    data = data[["Title",  "ECR%", "EPP%", "Greens/EFA%","EFD/IDG%",
+                 "NI%","REG%","S&D%", "The Left%", "General Majority", 
+                 "Left Majority", "Right Majority", "Consensus", "Summary text"
+                ]]
+    # sample the training data
+    training = data.sample(frac=training_fraction)
+    # the rest of the data is the testing data
+    testing = data.drop(training.index)
+    print(training.shape)
+    print(testing.shape)
+    return training, testing

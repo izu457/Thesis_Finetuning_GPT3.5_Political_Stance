a6117f5727c17149766a1347cb7794947915aaf2
izu457 <praecil@gmail.com>
2024-08-14 19:03:22 +0200

    cleaned repository

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Thesis_db
+
+This research analyses how fine-tuning can improve the ability of ChatGPT3.5 to detect and predict political stance in legislation and to further generate amendments of counterfactual political stance.
+Legislation voted on by the European Parliament between 2009 and 2022 has been scraped from the Parliament's websites as summaries and combined with the voting records compiled by Simon Hix within the project VoteWatch.eu.
+ChatGPT3.5 has been trained on the summaries and voting records to recognize the supporting political majority and party percentages. High increase in accuracy was achieved when comparing to a baseline version of ChatGPT3.5.
+This research is part of a Master Thesis.
--- cookbook_function.py
+++ cookbook_function.py
@@ -17,7 +17,6 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # Authenticate
-os.environ['TestKey3'] = 'sk-proj-GL73kbRwhRpgN3EmXz1YT3BlbkFJEMJhTsinxQDel42BZdNz' 
 client = openai.OpenAI(api_key=os.environ['TestKey3'])
 headers = {
     "Authorization": f"Bearer {os.environ['TestKey3']}"
@@ -227,48 +226,13 @@ def extract_job_info(all_job_ids):
     results_df = pd.DataFrame(results)
     return results_df
 
-# def get_ft_results(file_id):
-#     """
-#     Given a result files id of a finished fine-tuning job, a request is made to the OpenAI API
-#     to retrieve the content of the file. Content is decoded from Base64 and saved to a 
-#     csv file, which can be later loaded as a pandas DataFrame.
-#     """
-#     headers = {'Authorization': 'Bearer sk-proj-GL73kbRwhRpgN3EmXz1YT3BlbkFJEMJhTsinxQDel42BZdNz'}
-#     try:
-#         response = requests.get(f"https://api.openai.com/v1/files/{file_id}/content", headers=headers)
-#         # print the url of the request
-#         print(response.url)
-#         response.raise_for_status()  # Raises HTTPError for bad responses (4xx and 5xx)
-#         logger.info("Received response for file content.")
-#         if response.content:
-#             try:
-#                 # Parse the JSON content
-#                 decoded_content = base64.b64decode(response.content).decode('utf-8')
-#                 decoded_content = fix_base64_padding(decoded_content)
-#                 logger.info("Parsed JSON content successfully.")
-#                 with open("decoded_content.csv", "w") as f:
-#                     f.write(decoded_content)
-#                 logger.info("File 'decoded_content.csv' written successfully.")
-#             except (ValueError, base64.binascii.Error) as e:
-#                 # Handle the case where the response is not valid JSON
-#                 logger.error("Response content could not be parsed as JSON")
-#                 logger.error(f"Exception: {e}")
-#                 logger.error(response.content)
-#         else:
-#             logger.error("Response content is empty")
-#     except requests.exceptions.HTTPError as http_err:
-#         logger.error(f"HTTP error occurred: {http_err}")
-#     except requests.exceptions.RequestException as err:
-#         logger.error(f"Error occurred: {err}")
-#     return "decoded_content.csv"
-
 def get_ft_results(file_id):
     """
     Given a result files id of a finished fine-tuning job, a request is made to the OpenAI API
     to retrieve the content of the file. Content is decoded from Base64 and saved to a 
     csv file, which can be later loaded as a pandas DataFrame.
     """
-    headers = {'Authorization': 'Bearer sk-proj-GL73kbRwhRpgN3EmXz1YT3BlbkFJEMJhTsinxQDel42BZdNz'}
+    headers = {'Authorization': f'Bearer {os.environ["TestKey3"]}'}
     try:
         response = requests.get(f"https://api.openai.com/v1/files/{file_id}/content", headers=headers)
         # print the url of the request
@@ -300,7 +264,7 @@ def get_checkpoint_results(job_id):
     to retrieve the checkpoints which have been saved during the fine-tuning process. 
     Content is decoded from Base64 and saved to a string.
     """
-    headers = {'Authorization': f'Bearer sk-proj-GL73kbRwhRpgN3EmXz1YT3BlbkFJEMJhTsinxQDel42BZdNz'}
+    headers = {'Authorization': f'Bearer {os.environ["TestKey3"]}'}
     try:
         response = requests.get(f"https://api.openai.com/v1/fine_tuning/jobs/{job_id}/checkpoints", headers=headers)
         response.raise_for_status()  # Raises HTTPError for bad responses (4xx and 5xx)
